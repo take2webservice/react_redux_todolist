cc9faf1e4304b162353918ec1adf101fc2095e68
Taketo Iseki <take2webservice@gmail.com>
2021-01-21 16:52:49 +0900

    monolithic react todo

--- src/App.js
+++ src/App.js
@@ -1,58 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [
+        'hoge',
+        'fuga',
+        'boke'
+      ],
+      input: ''
+    }
+  }
+
+  onInputChange = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
+  }
+
+  addToDo = (e) => {
+    this.setState(() => {
+      return {
+        todos: [
+          ...this.state.todos,
+          this.state.input,
+        ],
+        input: ''
+      }
+    });
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <h1>TODO LIST</h1>
+        <div>
+          <input
+            onChange={this.onInputChange}
+            value={this.state.input}
+          />
+          <button className="add-todo" onClick={this.addToDo}>
+            Add Todo
+          </button>
+        </div>
+        <ul className="todo-list">
+          {this.state.todos.map(e => {
+            return <li className="todo-item">
+                     <span>
+                       {e}
+                     </span>
+                   </li>
+          })}
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+export default  App;
